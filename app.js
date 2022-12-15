7313138313d3622272cc43c62e17100830a6be96
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-12-15 16:42:53 +0100

    sans favicon (app.js)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 //const morgan = require('morgan')
-const favicon = require('serve-favicon');
+//const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const sequelize = require('./src/db/sequelize')
  
@@ -8,12 +8,17 @@ const app = express()
 const port = process.env.PORT || 3000
  
 app
-.use(favicon(__dirname + '/favicon.ico'))
+//.use(favicon(__dirname + '/favicon.ico'))
 //.use(morgan('dev'))
 .use(bodyParser.json())
  
 sequelize.initDb()
  
+// test HEROKU
+app.get('/', (req, res) => {
+  res.json('Hello, Heroku ! <img draggable="false" role="img" class="emoji" alt="" src="https://s.w.org/images/core/emoji/13.0.1/svg/1f44b.svg">')
+})
+
 // Ici, nous placerons nos futurs points de terminaisons !
 require('./src/routes/findAllPokemons')(app)
 require('./src/routes/findPokemonByPk')(app)
