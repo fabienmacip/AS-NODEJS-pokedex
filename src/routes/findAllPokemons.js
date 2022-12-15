7e069588a1ba46ff3c809cf89d317900e5cb06de
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2022-12-15 14:50:41 +0100

    GET All avec name = mot précis

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -2,15 +2,25 @@ const { Pokemon } = require('../db/sequelize')
  
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    if(req.query.name) {
+      const name = req.query.name
+      return Pokemon.findAll({ where: { name: name } })
       .then(pokemons => {
-        const message = 'La liste des pokémons a bien été récupérée.'
-        res.json({ message, data: pokemons })
+        const message = `Il y a ${pokemons.length} pokémon(s) qui correspondent au terme de recherche ${name}.`
+        return res.json({ message, data: pokemons })
       })
-      .catch(error => {
-        const message = `La liste des pokémons n'a pas pu être récupérée. 
-                         Réessayez dans quelques instants.`
-        res.status(500).json({ message, data: error })
-      })
+    } 
+    else {
+      Pokemon.findAll()
+        .then(pokemons => {
+          const message = 'La liste des pokémons a bien été récupérée.'
+          res.json({ message, data: pokemons })
+        })
+        .catch(error => {
+          const message = `La liste des pokémons n'a pas pu être récupérée. 
+                          Réessayez dans quelques instants.`
+          res.status(500).json({ message, data: error })
+        })
+    }
   })
 }
